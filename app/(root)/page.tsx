8989d12c233158426861e97849379e4bc91b83a9
khanhcutedz <nguyenvietkhanh24112001@gmail.com>
2024-03-11 19:56:43 +0700

    Modal componets

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,21 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
+"use client";
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 const SetupPage = () => {
+  const onOPen = useStoreModal((state) => state.onOpen);
+  const isOPen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if(!isOPen){
+      onOPen();
+    }
+  }, [isOPen, onOPen]);
+
   return (
     <div className="p-4">
-      <UserButton afterSignOutUrl="/" />
+      Root Page
     </div>
   );
 }
